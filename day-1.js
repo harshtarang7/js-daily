24e0b72af2f413c10fe7e6343f6d8c3a7fac31b9
harshtarang7 <tarangharsh7@gmail.com>
2025-04-13 09:09:44 +0530

    day-1

---
+++ day-1.js
@@ -0,0 +1,15 @@
+// topic 1 -variables
+// print variables in log
+var a = 5;
+let b = 10;
+const c = 20;
+
+console.log(a,b,c)
+
+// print sum of two variables
+console.log(a+b+c) //35
+
+// hoisting check
+console.log(x)
+var x = 5; // it will return undefined as the variable is defined after calling it
+
