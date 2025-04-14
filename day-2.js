1da846841f671f2a7d3e36950c735b25a4940dbe
harshtarang7 <tarangharsh7@gmail.com>
2025-04-14 12:42:20 +0530

    conditional statements and loops

---
+++ day-2.js
@@ -0,0 +1,34 @@
+// topic 1 - if
+// check if number is negative or positive 
+// it will check that if number is greater than 0 or positive
+let checkNumber = 20;
+if(checkNumber>0){
+    console.log('number is positive')
+}
+
+// topic 2 -if else
+// check number is even or odd
+let evenOrOdd = 23;
+
+if(evenOrOdd%2===0){
+    console.log(`even:${evenOrOdd}`)
+}else{
+    console.log(`odd: ${evenOrOdd}`)
+}
+
+// topic 3 else if 
+// transform numerical grade to a letter grade
+
+
+// topic 4 for loop 
+// print numbers 1-10 
+for(i=1;i<=10;i++){
+    console.log(i)
+}
+
+// print even numbers from 2-20
+for(i=2;i<=20;i++){
+    if(i%2===0){
+        console.log(`even:${i}`)
+    }
+}
